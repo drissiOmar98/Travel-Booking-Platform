2e247b495a6347c7576d309d6873d60a250d4bd9
DrissiOmar <omar.drissi@esprit.tn>
2024-09-27 12:19:24 +0100

    feat: implement  reactive toast notifications

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -6,11 +6,15 @@ import {FaIconLibrary} from "@fortawesome/angular-fontawesome";
 import {fontAwesomeIcons} from "./shared/font-awesome-icons";
 import {NavbarComponent} from "./layout/navbar/navbar.component";
 import {FooterComponent} from "./layout/footer/footer.component";
+import {ToastModule} from "primeng/toast";
+import {MessageService} from "primeng/api";
+import {ToastService} from "./layout/toast.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, Button, NavbarComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, Button, NavbarComponent, FooterComponent, ToastModule],
+  providers: [MessageService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -20,11 +24,29 @@ export class AppComponent implements OnInit {
   faIconLibrary = inject(FaIconLibrary);
   isListingView = true;
 
+  toastService = inject(ToastService);
+  messageService = inject(MessageService);
+
   ngOnInit(): void {
     this.initFontAwesome();
+    this.listenToastService();
   }
 
   private initFontAwesome() {
     this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
+
+  /**
+   * Subscribes to the toast service to listen for incoming toast messages.
+   * When a new message is received (and it's not the initial state), the message service adds it to the UI to notify the user.
+   */
+  private listenToastService() {
+    this.toastService.sendSub.subscribe({
+      next: newMessage => {
+        if(newMessage && newMessage.summary !== this.toastService.INIT_STATE) {
+          this.messageService.add(newMessage);
+        }
+      }
+    })
   }
 }
